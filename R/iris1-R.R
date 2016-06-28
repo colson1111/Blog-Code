3cb25391ada4bcc4b24b31e5acfb2d445a10bb9f
Craig <colson1111@gmail.com>
2016-06-28 00:35:47 -0400

    add R iris code

--- R/iris1-R.R
+++ R/iris1-R.R
@@ -1,9 +1,35 @@
-print(iris)
 
-install.packages(c("data.table","dplyr"))
+# install the dplyr package
+install.packages("dplyr")
 
-library(data.table)
+# load the dplyr package
 library(dplyr)
 
+# store iris data in a new dataframe
+iris_work <- iris
 
-iris2 <- iris[which(Species == "setosa" | Species = "versicolor")]
+# use dplyr filter and select functions with magrittr pipe operator to select rows and columns
+iris_work <- iris %>% 
+  filter(Species == "setosa" | Species == "versicolor") %>% 
+  select(c(Species,Sepal.Length, Sepal.Width))
+
+# what is iris_work?
+class(iris_work) # data.frame
+
+# what is the shape of iris_work?
+dim(iris_work) # 100 rows by 3 columns
+
+# plot the data
+install.packages("ggplot2")
+library(ggplot2)
+
+p1 <- ggplot(iris_work, aes(x = Sepal.Width, y = Sepal.Length)) +  # create ggplot object 
+  geom_point(aes(color = factor(Species))) +                       # define aesthetics of points, and color by Species
+  scale_color_discrete(name = "Species",                           # change default legend values
+                       labels = c("Setosa","Versicolor"))
+
+p1 # show plot
+
+
+
+
